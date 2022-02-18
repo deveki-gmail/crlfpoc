ddd66fa8eb6430996c943c08d3f85ce8b4be6e9f
arun <arun@gmail.com>
2022-02-18 05:57:00 +0000

    file changed

--- src/main/java/com/concretepage/servlets/HelloStateServlet.java
+++ src/main/java/com/concretepage/servlets/HelloStateServlet.java
@@ -33,7 +33,7 @@ public class HelloStateServlet extends HttpServlet   {
         //File file = ResourceUtils.getFile("abc.txt");
         //InputStream inPut = new FileInputStream(file);
         //InputStream inPut = getClass().getResourceAsStream("abc.txt");
-        Resource resource = new ClassPathResource("classpath:abc2.txt");
+        Resource resource = new ClassPathResource("classpath:gistfile1.txt");
         InputStream inPut = resource.getInputStream();
         ServletOutputStream outStream = response.getOutputStream();
 		
